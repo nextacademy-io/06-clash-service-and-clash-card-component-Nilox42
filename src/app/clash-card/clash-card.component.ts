757f6e6ad075074fa060ddf6706166ab2def390c
justice-tim.mauerman <justice-tim.mauerman@olymp.com>
2025-03-19 10:20:53 +0100

    06.01

---
+++ src/app/clash-card/clash-card.component.ts
@@ -0,0 +1,19 @@
+import { Component, computed, input } from '@angular/core';
+import { Clash } from '../clash';
+import { NgIf } from '@angular/common';
+import { ProfilePictureComponent } from '../profile-picture/profile-picture.component';
+
+@Component({
+  selector: 'app-clash-card',
+  imports: [NgIf, ProfilePictureComponent],
+  templateUrl: './clash-card.component.html',
+  styleUrl: './clash-card.component.css',
+})
+export class ClashCardComponent {
+  clash = input.required<Clash>();
+
+  participants = computed(() => [
+    this.clash().createdByPeer,
+    ...this.clash().participants,
+  ]);
+}
